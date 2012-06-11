96dc47f00269e881aeeff7d6338968740754eeda
horacio <horacio.gonzalez@gmail.com>
2012-06-11 06:29:36 +0200

    It works!!!!

--- AndroThriftChat/src/com/arkea/client/android/activities/ChatWindowActivity.java
+++ AndroThriftChat/src/com/arkea/client/android/activities/ChatWindowActivity.java
@@ -1,6 +1,9 @@
 package com.arkea.client.android.activities;
 
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TJSONProtocol;
@@ -11,11 +14,11 @@ import org.apache.thrift.transport.TTransportException;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -33,10 +36,16 @@ public class ChatWindowActivity extends Activity {
 	Utilisateur user = new Utilisateur();
 	
 	//List of Messages which will serve as list items
-	LinkedList<Message> listItems=new LinkedList<Message>();
+	List<Message> listItems=new LinkedList<Message>();
 
 	//Defining Message adapter which will handle data of ListView
 	MessageAdapter adapter;
+	
+	//TimerTask for scanning
+	TimerTask timerTask;
+	Timer timer= new Timer();
+	final Handler handler = new Handler();
+
 
 	/** Called when the activity is first created. */
     @Override
@@ -69,6 +78,26 @@ public class ChatWindowActivity extends Activity {
     }
     
     
+    @Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		stopScan();
+	}
+
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		startScan();
+	}
+
+
+	private void listenForMessages() {
+    	
+    }
+    
     private void sendMessage() {
     
     	Message message = new Message();
@@ -81,11 +110,12 @@ public class ChatWindowActivity extends Activity {
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
            new SendMessage(message).execute();
+           messageBox.setText("");
         } else {
                 //TO-DO
         }
     }
-    
+
     private class SendMessage extends AsyncTask<Void, Void, Void> {
         
     		Message message;
@@ -119,7 +149,6 @@ public class ChatWindowActivity extends Activity {
     				Log.e("Sending message", "Thrift exception", e);
     				return null;
     			}
-    	    	
     	    	return null;
 
     	    	
@@ -128,4 +157,71 @@ public class ChatWindowActivity extends Activity {
 
 
         }   
+
+    private void startScan() {
+    	timerTask = new TimerTask() {
+    		public void run() {
+    			handler.post(new Runnable() {
+    				public void run() {
+    					
+    	    	    	new ReadMessages().execute();
+    	    	    	adapter.notifyDataSetChanged();
+    					Log.d("TIMER", "Timer set off");
+    				}
+    			});
+    		}};
+
+
+    		timer.schedule(timerTask, 500, 5000); 
+
+    }
+
+    public void stopScan(){
+
+    	if(timerTask!=null){
+    		Log.d("TIMER", "timer canceled");
+    		timerTask.cancel();
+    	}
+
+    }
+    private class ReadMessages extends AsyncTask<Void, Void, Void> {
+        
+		Message message;
+	    
+	    
+		@Override
+		protected Void doInBackground(Void... params) {
+			TTransport transport;
+	    	TProtocol protocol;
+	    	ChatRoomService.Client client;
+	    	
+	    	try {
+				transport = new THttpClient("http://192.168.0.31:8080/ChatRoomServer/chatroom/");
+				protocol = new TJSONProtocol(transport);
+				client = new ChatRoomService.Client(protocol);
+				List<Message> receivedList = client.getListeMessage();
+				for (int i =listItems.size(); i< receivedList.size() ; i++) {
+					listItems.add(receivedList.get(i));
+				}
+				Log.d("Read messages", "Done");
+				StringBuilder sb = new StringBuilder();
+				for (Message item : listItems) {
+					sb.append("'").append(item.getUtilisateur().getPseudo()).append("' : '").append(item.getContenu()).append("'");
+				}
+				Log.d("Liste Messages", sb.toString());
+				return null;
+				
+			} catch (TTransportException e) {
+				Log.e("Read messages", "Thrift exception", e);
+				return null;
+			} catch (TException e) {
+				Log.e("Read messages", "Thrift exception", e);
+				return null;
+			} catch (Exception e) {
+				Log.e("Sending message", "Thrift exception", e);
+				return null;
+			}
+		}
+    }
 }
+
